dd0fa2eb90df7bc787df4de8554e4874bac7b1f1
Kumar, Anamika <Anamika.Kumar@west.com>
2020-02-17 20:49:31 +0530

    Updates

---
+++ precache-manifest.cfb87f24509f3a3926b4c38dd478d13d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c42a9745af8814ad5e12f87bf0b41721",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d59cdecd254a80075650",
+    "url": "/monsters-rolodex/static/css/main.e97bc8b3.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d59cdecd254a80075650",
+    "url": "/monsters-rolodex/static/js/main.28e0f6d0.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
